6a63641744a5d51dcf09db0ba887f890de3edd37
JuliQ89 <julian.singheiser.official@gmail.com>
2025-02-13 20:42:21 +0100

    update Modal Close Button

--- frontend/src/components/common/Modal.tsx
+++ frontend/src/components/common/Modal.tsx
@@ -14,7 +14,7 @@ const Modal = ({ children, isOpened, setIsOpened }: ModalI) => {
         <div className="fixed z-[999] top-0 left-0 right-0 bottom-0 bg-[rgba(30,41,59,0.8)] backdrop-blur-[2px] flex items-center justify-center">
           <div className="rounded-md border border-slate-200 bg-white p-6">
             <button
-              className="flex items-center justify-center rounded-sm bg-transparent border-none p-[0.35rem] outline-none cursor-pointer text-slate-800 hover:bg-gray-200 float-right"
+              className="flex items-center justify-center rounded-sm bg-transparent border-none p-[0.35rem] outline-none cursor-pointer mb-2 text-slate-800 hover:bg-gray-200 float-right"
               onClick={() => setIsOpened(false)}
             >
               <IoMdClose />
